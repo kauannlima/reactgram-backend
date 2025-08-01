7820eb366774549f3472189654b608a0fce60f70
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-07-31 21:30:58 -0300

    Finalziando backend

--- controllers/PhotoController.js
+++ controllers/PhotoController.js
@@ -197,6 +197,14 @@ const commentPhoto = async (req, res) => {
   });
 };
 
+// Search photos by title
+const searchPhotos = async (req, res) => {
+  const { q } = req.query;
+
+  const photos = await Photo.find({ title: new RegExp(q, "i") }).exec();
+  res.status(200).json(photos);
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
@@ -206,4 +214,5 @@ module.exports = {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 };
